e8091e5af3ebbcc7393fe7471cf7ed4e06fc2994
Tarique Hasan <39570587+tariquedev@users.noreply.github.com>
2024-11-30 13:17:02 +0600

    minore changes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ZoomController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/zoom/connect', [ZoomController::class, 'connect'])->name('zoom.connect');
+// Route::get('/zoom/callback', [ZoomController::class, 'callback'])->name('zoom.callback');
+// Route::post('/zoom/disconnect', [ZoomController::class, 'disconnect'])->name('zoom.disconnect');
+
+Route::get('/', [ZoomController::class, 'generateAuthLink'])->name('zoom.authLol');
+Route::get('/zoom/auth', [ZoomController::class, 'generateAuthLink'])->name('zoom.auth');
+Route::get('/zoom/callback', [ZoomController::class, 'handleCallback'])->name('zoom.callback');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
