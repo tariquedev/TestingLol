900f3c9f5d7b041cef408022be612273c2f0ecb3
Tarique Hasan <39570587+tariquedev@users.noreply.github.com>
2024-11-30 14:44:42 +0600

    minor changes

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,17 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/zoom/callback', [ZoomController::class, 'callback'])->name('zoom.callback');
 // Route::post('/zoom/disconnect', [ZoomController::class, 'disconnect'])->name('zoom.disconnect');
 
-Route::get('/', [ZoomController::class, 'generateAuthLink'])->name('zoom.authLol');
-Route::get('/zoom/auth', [ZoomController::class, 'generateAuthLink'])->name('zoom.auth');
-Route::get('/zoom/callback', [ZoomController::class, 'handleCallback'])->name('zoom.callback');
-Route::get('/zoom/disconnect', [ZoomController::class, 'disconnectZoomApp'])->name('zoom.disconnect');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', [ZoomController::class, 'generateAuthLink'])->name('zoom.authLol');
+    Route::get('/zoom/auth', [ZoomController::class, 'generateAuthLink'])->name('zoom.auth');
+    Route::get('/zoom/callback', [ZoomController::class, 'handleCallback'])->name('zoom.callback');
+    Route::get('/zoom/disconnect', [ZoomController::class, 'disconnectZoomApp'])->name('zoom.disconnect');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
